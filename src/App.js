e2fa868f97fcfe5b07d6380a7857afabc8bc8e46
Chandelier Axel <AxeletCana@hotmail.fr>
2019-04-26 22:42:13 +0200

    trying to solve cors

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
 
 componentDidMount(){
     const api = "http://localhost:8010/proxy/m=itemdb_rs/api/catalogue/detail.json?item="
-    const apiCharacter = `http://localhost:8011/proxy/profile/profile?user=${this.state.inputCharacter}&activities=20`
+    const apiCharacter = `https://apps.runescape.com/runemetrics/profile/profile?user=${this.state.inputCharacter}&activities=20`
       fetch(`${api}31725`)
         .then(response  => response.json())
         .then(data  => {this.setState({item: data.item})});
